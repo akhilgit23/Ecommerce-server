c761ec3b90c0e59058486bf872d58e78fe1a1f0c
Akhilraj <akhilakhilraj18@gmail.com>
2024-08-27 11:01:25 +0530

    update vercel.json

--- server.js
+++ server.js
@@ -7,21 +7,15 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 
-
-
 dotenv.config();
 
 const app = express()
 const port = process.env.PORT || 8080;
 
-
-
 //middlewares
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'));
-
-
 
 //routes
 app.use('/api/v1/auth', authRoutes);
