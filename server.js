07476008cd55a4c9c349773fcda6a2f207e608f4
Akhilraj <akhilakhilraj18@gmail.com>
2024-08-27 11:57:45 +0530

    update server.js

--- server.js
+++ server.js
@@ -13,9 +13,8 @@ const app = express()
 const port = process.env.PORT || 8080;
 
 //middlewares
-const cors = require('cors');
-app.use(cors({ origin: 'https://www.ecommerce-server-coral-two.vercel.app.com' }));
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(morgan('dev'));
 
 //routes
