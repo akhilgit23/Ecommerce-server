b93ef417857ada7bc64010bdb88906930d4136df
Akhilraj <akhilakhilraj18@gmail.com>
2024-08-30 12:34:55 +0530

    update server.js

--- server.js
+++ server.js
@@ -12,8 +12,21 @@ dotenv.config();
 const app = express()
 const port = process.env.PORT || 8080;
 
+// CORS configuration
+const allowedOrigins = ['https://ecommerce-server-coral-two.vercel.app'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 //middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('dev'));
 
